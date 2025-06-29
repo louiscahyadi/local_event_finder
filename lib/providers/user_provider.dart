0975a587b36f07d0eb0394ee0879db760c1ba0d9
louiscahyadi <loouis1911@gmail.com>
2025-06-30 04:50:07 +0800

    Ubah parameter metode login dan register dari username menjadi email, serta simpan email pengguna

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -3,29 +3,34 @@ import '../helpers/shared_pref_helper.dart';
 
 class UserProvider with ChangeNotifier {
   String? _username;
+  String? _userEmail;
 
   String? get username => _username;
+  String? get userEmail => _userEmail;
 
-  Future<void> login(String username, String password) async {
-    if (username == 'admin' && password == '1234') {
+  Future<void> login(String email, String password) async {
+    if (email == 'admin' && password == '1234') {
       // contoh hardcoded user
-      _username = username;
-      await SharedPrefHelper.saveUser(username);
+      _username = email;
+      _userEmail = email;
+      await SharedPrefHelper.saveUser(email);
       notifyListeners();
     } else {
       throw ('Username/Password salah');
     }
   }
 
-  Future<void> register(String username, String password) async {
+  Future<void> register(String email, String password) async {
     // simulasi register (tidak simpan ke DB)
-    _username = username;
-    await SharedPrefHelper.saveUser(username);
+    _username = email;
+    _userEmail = email;
+    await SharedPrefHelper.saveUser(email);
     notifyListeners();
   }
 
   Future<void> logout() async {
     _username = null;
+    _userEmail = null;
     await SharedPrefHelper.logout();
     notifyListeners();
   }
