94421b6a79dd81b195ff3a5ab326d3b9c3c12635
louiscahyadi <loouis1911@gmail.com>
2025-06-30 04:47:26 +0800

    Ubah tipe data id menjadi int dan tambahkan kategori ke kelas Event

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,10 +1,12 @@
 class Event {
-  final String? id;
+  final int? id;
   final String title;
   final String description;
   final String date;
   final double latitude;
   final double longitude;
+  final String? imagePath; // pastikan ada
+  final String? kategori; // tambahkan ini
 
   Event({
     this.id,
@@ -13,18 +15,22 @@ class Event {
     required this.date,
     required this.latitude,
     required this.longitude,
+    this.imagePath,
+    this.kategori, // tambahkan ini
   });
 
   String get location => '$latitude, $longitude';
 
   factory Event.fromMap(Map<String, dynamic> map) {
     return Event(
-      id: map['id']?.toString(),
+      id: map['id'],
       title: map['title'],
       description: map['description'],
       date: map['date'],
       latitude: map['latitude'],
       longitude: map['longitude'],
+      imagePath: map['imagePath'],
+      kategori: map['kategori'], // tambahkan ini
     );
   }
 
@@ -36,6 +42,8 @@ class Event {
       'date': date,
       'latitude': latitude,
       'longitude': longitude,
+      'imagePath': imagePath,
+      'kategori': kategori, // tambahkan ini
     };
   }
 }
